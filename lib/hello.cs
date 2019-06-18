98d6eedc39fb2a2ffb9ed5f4dedf51bdf854f821
tavisca-akadam <akadam@tavisca.in>
2019-06-18 15:22:34 +0530

    Hello uses greeter

--- lib/hello.cs
+++ lib/hello.cs
@@ -1,13 +1,17 @@
 using System;
+using ConsoleApp1.greeter;
 
 namespace HelloWorldApplication {
     // Author : Anil Kadam (akadam@tavisca.in)
    class HelloWorld {
       static void Main(string[] args) {
-        //Taking input from user
-        string name = Console.ReadLine(); 
-        Console.WriteLine("Hello {0}", name);
-        Console.ReadKey();
+      //Taking input from user
+      string name = Console.ReadLine(); 
+      // Creating object of greeter class
+      greeter greet = new greeter(name);
+      Console.WriteLine(greet.greetMsg());
+
+      Console.ReadKey();
       }
    }
 }
