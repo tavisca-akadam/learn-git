08e147d45a13a39ebe7e3b5065ff4fbb60a99fa1
tavisca-akadam <akadam@tavisca.in>
2019-06-18 15:17:32 +0530

    Added greeter class

---
+++ lib/greeter.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class greeter
+    {
+        public string msg = "World";
+
+        public Class1(string name)
+        {
+            msg = name;
+        }
+        public string greetMsg()
+        {
+            return "Hello " + msg;
+        }
+    }
+}
