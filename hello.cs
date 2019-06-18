0653c0f9c8781a3d86c9e244582146f0dae69af8
tavisca-akadam <akadam@tavisca.in>
2019-06-18 11:53:49 +0530

    Adding comment in program

--- hello.cs
+++ hello.cs
@@ -3,6 +3,7 @@ using System;
 namespace HelloWorldApplication {
    class HelloWorld {
       static void Main(string[] args) {
+        //Taking input from user
         string name = Console.ReadLine(); 
         Console.WriteLine("Hello {0}", name);
         Console.ReadKey();
